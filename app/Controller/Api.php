593c0b21f99e2c0aee9e2540ab04207521a848f6
Cloud Bill Master <strblackhawk@gmail.com>
2025-03-08 00:19:19 +0600

    404 moved to App and Controller Modified

---
+++ app/Controller/Api.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Project: Laika MVC Framework
+ * Author Name: Showket Ahmed
+ * Author Email: riyadhtayf@gmail.com
+ */
+
+// Namespace
+namespace CBM\App\Controller;
+
+// Forbidden Access
+defined('ROOTPATH') || http_response_code(403).die('403 Forbidden Access!');
+
+/**
+* class Index Extends Controller
+* {
+*    public function index()
+*    {
+*        $data['title'] = 'Laika Home Page';
+
+*        //// For Middleware
+*        // $this->middleware('class', 'method', []);
+*        //// For View
+*        $this->view('index', $data);
+*    }
+* }
+*/
+
+class Api
+{
+    //
+}
