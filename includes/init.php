b089a51c58086f7124465fb1f4fc024bfafa9670
cb-master <strblackhawk@gmail.com>
2025-05-21 00:44:42 +0600

    Init Updated

--- includes/init.php
+++ includes/init.php
@@ -5,6 +5,7 @@ defined('ROOTPATH') || http_response_code(403).die('Direct Access Denied!');
 
 use CBM\Core\ErrorHandler\ErrorHandler;
 use CBM\Core\Directory\Directory;
+use CBM\Core\Response\Response;
 use CBM\Core\Config\Config;
 
 require(__DIR__.'/../vendor/autoload.php');
@@ -15,9 +16,15 @@ Config::set(Directory::files(ROOTPATH.'/configs', 'php'));
 // Register Error Handler
 ErrorHandler::register(Config::get('app', 'debug'));
 
+// Set Response Headers
+Response::header();
+
 // Autoload System Files
 array_map(function($folder){
     array_map(function($file){
         require_once $file;
     }, Directory::files($folder, 'php'));
 }, Directory::folders(ROOTPATH.'/system'));
+
+// Load postload file if custom filters/functions/clases to load
+require_once(ROOTPATH.'/system/custom.php');
