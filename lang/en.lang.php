15589d9c5353c0ce6d43a7ed5d4da722b66bf9b4
cb-master <strblackhawk@gmail.com>
2025-05-21 01:15:15 +0600

    lang folder added

---
+++ lang/en.lang.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Project: Laika MVC Framework
+ * Author Name: Showket Ahmed
+ * Author Email: riyadhtayf@gmail.com
+ */
+
+// Forbidden Access
+defined('ROOTPATH') || http_response_code(403).die('403 Forbidden Access!');
+
+// English Language Class
+class LANG
+{
+    // Declaer Static Language Variables.
+    
+    static $sample = "Sample Text!";
+}
