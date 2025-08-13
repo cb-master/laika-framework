ce5ca8bcf5560cc4607236a4817357fb9133ee4a
cb-master <strblackhawk@gmail.com>
2025-08-14 02:01:29 +0600

    memcached config added

---
+++ config/memcached.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Laika PHP MVC Framework
+ * Author: Showket Ahmed
+ * Email: riyadhtayf@gmail.com
+ * License: MIT
+ * This file is part of the Laika PHP MVC Framework.
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+// Deny Direct Access
+defined('BASE_PATH') || http_response_code(403).die('403 Direct Access Denied!');
+
+// Memcached Config
+return [
+    // Host
+    'host'      =>  '127.0.0.1',
+
+    // Port
+    'port'      =>  11211,
+
+    // Prefix
+    'prefix'    =>  'cbm',
+
+    // Username
+    'username'  =>  '',
+
+    // Password
+    'password'  =>  ''
+];
